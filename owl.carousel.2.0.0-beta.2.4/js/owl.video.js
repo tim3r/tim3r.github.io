94888bf24edf805a6b3b0df984362a20a9a7b64a
tim3r <tim3r@users.noreply.github.com>
2020-06-17 00:44:32 +0300

    Update owl.video.js

--- owl.carousel.2.0.0-beta.2.4/js/owl.video.js
+++ owl.carousel.2.0.0-beta.2.4/js/owl.video.js
@@ -233,7 +233,7 @@
 		this._core.reset(item.index());
 
 		if (video.type === 'youtube') {
-			html = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' +
+			html = '<iframe width="' + width + '" height="' + height + '" src="https://www.youtube.com/embed/' +
 				video.id + '?autoplay=1&v=' + video.id + '" frameborder="0" allowfullscreen></iframe>';
 		} else if (video.type === 'vimeo') {
 			html = '<iframe src="http://player.vimeo.com/video/' + video.id +
